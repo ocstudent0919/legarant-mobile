b9a323982277b22a5950c31d63d4ad7077c0185a
ocstudent0919 <ocstudent0919@protonmail.com>
2021-12-30 20:22:05 +0100

    Update date

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('PORT', process.env.PORT || 3000)
 app.get('/', (req, res) => {
     res.status(200).send({
         message: 'How do I deploy my app to Heroku today?',
-        date: new Date().toDateString(),
+        date: new Date().toLocaleString(),
     })
 })
 
