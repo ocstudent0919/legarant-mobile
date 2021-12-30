f30c17433e45c66c626a25eeaf3a035c55d9fa5c
ocstudent0919 <ocstudent0919@protonmail.com>
2021-12-30 16:17:55 +0100

    Add date to index.js message

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set('PORT', process.env.PORT || 3000)
 app.get('/', (req, res) => {
     res.status(200).send({
         message: 'How do I deploy my code to Heroku today?',
+        date: new Date().toDateString(),
     })
 })
 
